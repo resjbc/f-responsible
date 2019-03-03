a367a6df8bf3a94170c4c56326fed300b2c1ce20
Hitman <31611759+resjbc@users.noreply.github.com>
2019-03-03 22:58:21 +0700

    95

--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -6,32 +6,44 @@ import { ManagementUserComponent } from './components/management-user/management
 import { ProfileComponent } from './components/profile/profile.component';
 import { AddHeadWorkComponent } from './components/add-head-work/add-head-work.component';
 import { AddPositionComponent } from './components/add-position/add-position.component';
+import { ERoleAccount } from '../components/login/login.interface';
+import { UserRoleGuard } from '../guards/user-role.guard';
 
 const RouterList: Routes = [
     { path: '', redirectTo: AuthURL.AddCatchmentAreaComponent, pathMatch: 'full' },
-    { 
-        path: AuthURL.AddCatchmentAreaComponent, 
-        component: AddCatchmentAreaComponent ,
+    {
+        path: AuthURL.AddCatchmentAreaComponent,
+        component: AddCatchmentAreaComponent,
+        canActivate: [UserRoleGuard] ,
+        data: { roles: [ERoleAccount.Member, ERoleAccount.Admin] }
     },
-    { 
-        path: AuthURL.SearchResponsiblesComponent, 
-        component: SearchResponsiblesComponent ,
+    {
+        path: AuthURL.SearchResponsiblesComponent,
+        component: SearchResponsiblesComponent,
+        canActivate: [UserRoleGuard] ,
+        data: { roles: [ERoleAccount.Employee, ERoleAccount.Admin] }
     },
-    { 
-        path: AuthURL.ManagementUserComponent, 
-        component: ManagementUserComponent 
+    {
+        path: AuthURL.ManagementUserComponent,
+        component: ManagementUserComponent,
+        canActivate: [UserRoleGuard] ,
+        data: { roles: [ERoleAccount.Admin] }
     },
-    { 
-        path: AuthURL.ProfileComponent, 
-        component: ProfileComponent 
+    {
+        path: AuthURL.ProfileComponent,
+        component: ProfileComponent
     },
-    { 
-        path: AuthURL.AddHeadWorkComponent, 
-        component: AddHeadWorkComponent 
+    {
+        path: AuthURL.AddHeadWorkComponent,
+        component: AddHeadWorkComponent,
+        canActivate: [UserRoleGuard] ,
+        data: { roles: [ERoleAccount.Employee, ERoleAccount.Admin] }
     },
-    { 
-        path: AuthURL.AddPositionComponent, 
-        component: AddPositionComponent 
+    {
+        path: AuthURL.AddPositionComponent,
+        component: AddPositionComponent,
+        canActivate: [UserRoleGuard] ,
+        data: { roles: [ERoleAccount.Employee, ERoleAccount.Admin] }
     },
 ];
 
